43849eb141cf0548b1956a9196d7cfe07ebbb334
Annalakshmi Jeyashingh <ajeyashi@localhost>
2017-05-15 16:53:02 +0530

    small msg

--- warrior/Framework/Utils/cli_Utils.py
+++ warrior/Framework/Utils/cli_Utils.py
@@ -1,3 +1,4 @@
+https://askubuntu.com/questions/661186/how-to-install-previous-firefox-version
 '''
 Copyright 2017, Fujitsu Network Communications, Inc.
 Licensed under the Apache License, Version 2.0 (the "License");
