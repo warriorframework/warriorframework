47c209c2edb8a6c933be5c0dd1b832eb1038a7c7
kaheichan <kaheichan@utexas.edu>
2017-06-19 18:42:48 -0500

    verifying pylint

--- wftests/travis/pylint.sh
+++ wftests/travis/pylint.sh
@@ -29,6 +29,7 @@ do
     line_status="pass"
     num1=$(grep -oP "at \K[0-9\.\-]*" <<< "$line");
     num2="5";
+    echo "$num1 < $num2" | bc
     if [[ $(echo "$num1 < $num2" | bc) -ne 0 ]] ; then
         echo "score lower than 5: $num1"
         status="fail"
@@ -37,6 +38,7 @@ do
     
     num3=$(grep -oP "previous run.*/10, \K[0-9\.\-+]*" <<< "$line" | tr -d "+")
     num4="0"
+    echo "$num3 < $num4"
     if [[ $(echo "$num3 < $num4" | bc) -ne 0 ]] ; then
         echo "score decrease: $num3"
         status="fail"
