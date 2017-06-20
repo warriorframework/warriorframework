90747153efef54155ca6df59dffff9ff7705a01b
kaheichan <kaheichan@utexas.edu>
2017-06-20 14:11:07 -0500

    fixed the shell script

--- wftests/travis/pylint.sh
+++ wftests/travis/pylint.sh
@@ -2,6 +2,7 @@
 # ls -l
 pip install pylint
 git checkout master
+# Displaying what .py files have changed
 if [[ $(git --no-pager diff --name-only master "${TRAVIS_COMMIT}"  | grep -v 'OSS' | grep '.py$') ]]; then
     echo "List of .py files that have changed in this commit"
     git --no-pager diff --name-only master "${TRAVIS_COMMIT}"  | grep -v 'OSS' | grep '.py$'
@@ -9,11 +10,13 @@ else
     echo "no .py file has changed in this commit, exiting"
     exit 0;
 fi
-git --no-pager diff --name-only master "${TRAVIS_COMMIT}"  | grep -v 'OSS' | grep '.py$' | xargs pylint || true
+# Do pylint on master and the latest commit, output result to pylint_result.txt
+git --no-pager diff --name-only master "${TRAVIS_COMMIT}"  | grep -v 'OSS' | grep '.py$' | xargs -L 1 pylint || true
 git checkout "${TRAVIS_COMMIT}" ;
 git --no-pager diff --name-only "${TRAVIS_COMMIT}" master  | grep -v 'OSS' | grep '.py$'
-git --no-pager diff --name-only "${TRAVIS_COMMIT}" master  | grep -v 'OSS' | grep '.py$' | xargs pylint | tee pylint_result.txt || true
+git --no-pager diff --name-only "${TRAVIS_COMMIT}" master  | grep -v 'OSS' | grep '.py$' | xargs -L 1 pylint | tee pylint_result.txt || true
 
+# Match filename and score into summary.txt
 grep "Your code has been rated" pylint_result.txt > score.txt
 grep "Module" pylint_result.txt > filename.txt
 
@@ -21,6 +24,7 @@ sed -i -e 's/\** Module//g' filename.txt
 sed -i -e 's/Your code//g' score.txt
 paste -d "^" filename.txt score.txt | column -t -s "^" | tee summary.txt
 
+# Check if there is an increase 
 set +x
 echo "Files that doesn't meet the pylint score requirement (>5 with score increase)"
 status="pass"
@@ -30,9 +34,6 @@ do
     echo "$line"
     num1=$(grep -oP "at \K[0-9\.\-]*" <<< "$line");
     num2="5";
-    echo "$num1"
-    echo "$num2"
-    echo "$num1 < $num2" | bc
     if [[ $(echo "$num1 < $num2" | bc) -ne 0 ]] ; then
         echo "score lower than 5: $num1"
         status="fail"
@@ -41,16 +42,13 @@ do
     
     num3=$(grep -oP "previous run.*/10, \K[0-9\.\-+]*" <<< "$line" | tr -d "+")
     num4="0"
-    echo "$num3"
-    echo "$num4"
-    echo "$num3 < $num4"
     if [[ $(echo "$num3 < $num4" | bc) -ne 0 ]] ; then
         echo "score decrease: $num3"
         status="fail"
         line_status="fail"
     fi
 
-    if [ "$line_status" = "fail" ]; then
+    if [[ "$line_status" = "fail" ]]; then
         filename=$(grep -oP "[^\s]{2,}" <<< "$line" | head -1)
         echo "$filename ($num1, $num3)"
     fi
