cd66fa8c01ce697c607353076ce642a85ac33b46
kaheichan <kaheichan@utexas.edu>
2017-06-21 15:36:06 -0500

    finish logical decision for rule

--- warrior/WarriorCore/exec_type_driver.py
+++ warrior/WarriorCore/exec_type_driver.py
@@ -45,7 +45,7 @@ def math_decision(exec_condition, exec_cond_var, operator):
     pNote("Unknown error occur when deciding value, please check condition value of the step", "Error")
     return False
 
-def logical_decision(exec_type, exec_condition, exec_cond_var, operator):
+def logical_decision(exec_type, exec_condition, exec_cond_var, operator="equal"):
     """
         Handle the logical decision for the value comparison
         :param:
@@ -56,32 +56,29 @@ def logical_decision(exec_type, exec_condition, exec_cond_var, operator):
         :return:
             True if condition match, else return False
     """
-    status = None
+    status = True
+    result = None
     if type(get_object_from_datarepository(exec_condition)) != type(exec_cond_var):
-        print_error("Comparing different type of value, please check the conditional value type")
+        pNote("Comparing different type of value, please check the conditional value type", "ERROR")
+        status = False
+    elif operator in ["greater_equal", "greater", "smaller_equal", "smaller"] and\
+         not isinstance(exec_condition, int) and not isinstance(exec_condition, float):
+        pNote("Comparing non-numerical value using numerical operator, please check value and operator type", "ERROR")
         status = False
 
-    if status is None and operator is not None:
+    if status and operator == "equal":
+        result = True if get_object_from_datarepository(exec_condition) == exec_cond_var else False
+    elif status and operator == "not_equal":
+        result = True if get_object_from_datarepository(exec_condition) != exec_cond_var else False
+    elif status and operator in ["greater_equal", "greater", "smaller_equal", "smaller"]:
         result = math_decision(exec_condition, exec_cond_var, operator)
-        status = result if exec_type.upper() == 'IF' else not result
-
-    if status is None and exec_type.upper() == 'IF':
-        status = True if get_object_from_datarepository(exec_condition) == exec_cond_var else False
-        
-    elif status is None and exec_type.upper() == 'IF NOT':
-        status = False if get_object_from_datarepository(exec_condition) == exec_cond_var else True
-    
-    elif status is None:
-        supported_values = ['no', 'yes', 'if', 'if not']
-        print_error("Unsupported value used for ExecType, supported values are: {0} and case-insensitive".format(supported_values))
-        status = False
-
-    if not status:
+    else:
         pNote("Execution condition failed for exec type: {}, "\
-            "expected value: {} , condition: {}, actual value: {}"\
-            .format(exec_type, exec_cond_var, operator,
-                    get_object_from_datarepository(exec_condition)), "WARNING")
-    return status if status is not None else False
+              "expected value: {} , condition: {}, actual value: {}"\
+              .format(exec_type, exec_cond_var, operator,
+                      get_object_from_datarepository(exec_condition)), "WARNING")
+
+    return result
 
 # Expression parsing and handling
     # parse (),each parenthesis becomes a new layer of conditions
@@ -94,6 +91,88 @@ def logical_decision(exec_type, exec_condition, exec_cond_var, operator):
     # Once else is triggered, build the return status and action
 # AND/OR logic handling
 
+def rule_parser(rule):
+    # Get data
+    exec_condition = rule.get("Condition", None)
+    exec_cond_var = rule.get("Condvalue", None)
+    else_action = rule.get("Else", None)
+    if else_action is not None and else_action.upper() == "GOTO":
+        else_action = rule.get("Elsevalue")
+
+    # Check for math operator
+    support_operators = ["greater_equal", "greater", "smaller_equal", "smaller", "equal", "not_equal"]
+    operator = rule.get("Operator", None)
+    if operator is not None and operator.lower() not in support_operators:
+        pNote("Invaid Operator value, please use the following: {}".format(support_operators))
+        operator = None
+
+    # Check for value prefix
+    supported_prefix = ["bool_", "str_", "int_", "float_", "list_", "tuple_", "dict_"]
+    if any([exec_condition.startswith(i) for i in supported_prefix]):
+        exec_condition = exec_condition[exec_condition.find('_')+1:]
+
+    arg_datatype_object = ArgumentDatatype(exec_condition, exec_cond_var)
+    exec_cond_var = arg_datatype_object.convert_arg_to_datatype()
+
+    status = logical_decision(exec_type, exec_condition, exec_cond_var, operator)
+
+def simple_expression_parser(expression_str):
+    elements = expression_str.split()
+    if len(elements) == 0:
+        # illegal expression
+        raise Exception("element ")
+    elif len(elements) == 1:
+        return rule_parser(elements[0])
+    else:
+        status = rule_parser(elements[0])
+        for x in range(1, len(elements) - 2):
+            if elements[x+1].lower() == "and" or elements[x+1] == "&":
+                status = status & rule_parser(elements[x+2])
+            elif elements[x+1].lower() == "or" or elements[x+1] == "|":
+                status = status | rule_parser(elements[x+2])
+            else:
+                # invalid operator
+                raise Exception
+
+def expression_parser(expression_str):
+    return True
+
+def decision_maker(exec_node):
+    decision = True
+    exec_type = exec_node.get("ExecType", "")
+    expression = exec_node.get("Expression", "")
+    rules = exec_node.findall("Rule")
+    # Sanity check for multiple else
+
+    else_node = exec_node.find("./Rule[@Else]") if exec_node.find("./Rule[@Else]") is not None else {}
+    else_action = else_node.get("Else", None)
+    if else_action is not None and else_action.upper() == "GOTO":
+        else_action = else_node.get("Elsevalue")
+
+    for rule in rules:
+        exec_condition = rule.get("Condition", None)
+        exec_cond_var = rule.get("Condvalue", None)
+
+        # Check for math operator
+        support_operators = ["greater_equal", "greater", "smaller_equal", "smaller"]
+        operator = rule.get("Operator", None)
+        if operator is not None and operator.lower() not in support_operators:
+            pNote("Invaid Operator value, please use the following: {}".format(support_operators))
+            operator = None
+
+        arg_datatype_object = ArgumentDatatype(exec_condition, exec_cond_var)
+        exec_cond_var = arg_datatype_object.convert_arg_to_datatype()
+
+        supported_prefix = ["bool_", "str_", "int_", "float_", "list_", "tuple_", "dict_"]
+        if any([exec_condition.startswith(i) for i in supported_prefix]):
+            exec_condition = exec_condition[exec_condition.find('_')+1:]
+
+        decision = decision and logical_decision(exec_type, exec_condition, exec_cond_var, operator)
+
+    if not decision:
+        trigger_action = else_action if else_action is not None else "SKIP"
+        pNote("Failed action: {}".format(trigger_action))
+
 def main(step):
     """
         Entry function for execute nodes in a step
@@ -105,75 +184,21 @@ def main(step):
             decision: Whether the step should be executed or not
             trigger_action: When decision failed, what kind of action to perform
     """
-    exec_nodes = step.findall("Execute")
-    if exec_nodes == []:
-        return True, None
-    if len(exec_nodes) > 1:
-        exec_types = [node.get("ExecType").upper() for node in exec_nodes if node.get("ExecType") is not None]
-
-        if "YES" in exec_types and "NO" in exec_types:
-            pNote("Multiple conflicted ExecType conditions found, please check xml", "WARNING")
-            pNote("Cannot have YES and NO statement together", "WARNING")
-            pNote("Proceed with using the 1st ExecType condition", "WARNING")
-            exec_nodes = [exec_nodes[0]]
-        if any(["IF" in exec_types or "IF NOT" in exec_types]) and any(["YES" in exec_types or "NO" in exec_types]):
-            pNote("Multiple conflicted ExecType conditions found, please check xml", "WARNING")
-            pNote("Cannot have YES/NO with any IF statement", "WARNING")
-            pNote("Proceed with using the 1st ExecType condition", "WARNING")
-            exec_nodes = [exec_nodes[0]]
-        if len(exec_types) != len(set(exec_types)):
-            pNote("Multiple same ExecType conditions found", "WARNING")
-            pNote("Proceed with using the 1st ExecType condition", "WARNING")
-            exec_nodes = [exec_nodes[0]]
+    exec_node = step.find("Execute")
+    if exec_node == []:
+        return False, None
 
     decision = True
     trigger_action = None
-    for exec_node in exec_nodes:
-        exec_type = exec_node.get("ExecType", "")
-        if exec_type.upper() == 'IF' or exec_type.upper() == 'IF NOT':
-            rules = exec_node.findall("Rule")
-            # Sanity check for multiple else
-            if len([True for rule in rules if rule.get("Else", None) is not None]) > 1:
-                pNote("Multiple else statement find in rules", "WARNING")
-                pNote("Proceed with using the 1st else statement", "WARNING")
-
-            else_node = exec_node.find("./Rule[@Else]") if exec_node.find("./Rule[@Else]") is not None else {}
-            else_action = else_node.get("Else", None)
-            if else_action is not None and else_action.upper() == "GOTO":
-                else_action = else_node.get("Elsevalue")
-
-            for rule in rules:
-                exec_condition = rule.get("Condition", None)
-                exec_cond_var = rule.get("Condvalue", None)
-
-                # Check for math operator
-                support_operators = ["greater_equal", "greater", "smaller_equal", "smaller"]
-                operator = rule.get("Operator", None)
-                if operator is not None and operator.lower() not in support_operators:
-                    pNote("Invaid Operator value, please use the following: {}".format(support_operators))
-                    operator = None
-
-                arg_datatype_object = ArgumentDatatype(exec_condition, exec_cond_var)
-                exec_cond_var = arg_datatype_object.convert_arg_to_datatype()
-
-                supported_prefix = ["bool_", "str_", "int_", "float_", "list_", "tuple_", "dict_"]
-                if any([exec_condition.startswith(i) for i in supported_prefix]):
-                    exec_condition = exec_condition[exec_condition.find('_')+1:]
-
-                decision = decision and logical_decision(exec_type, exec_condition, exec_cond_var, operator)
-
-            if not decision:
-                trigger_action = else_action if else_action is not None else "SKIP"
-                pNote("Failed action: {}".format(trigger_action))
-                break
-        elif exec_type.upper() == 'NO':
-            decision = False
-            trigger_action = "SKIP"
-            break
-        elif exec_type.upper() != 'YES':
-            decision = False
-            supported_values = ['no', 'yes', 'if', 'if not']
-            print_error("Unsupported value used for ExecType, supported values are: {0} and case-insensitive".format(supported_values))
-            break
+    exec_type = exec_node.get("ExecType", "")
+    if exec_type.upper() == 'IF' or exec_type.upper() == 'IF NOT':
+        decision, trigger_action = decision_maker(exec_node)
+    elif exec_type.upper() == 'NO':
+        decision = False
+        trigger_action = "SKIP"
+    elif exec_type.upper() != 'YES':
+        decision = False
+        supported_values = ['no', 'yes', 'if', 'if not']
+        print_error("Unsupported value used for ExecType, supported values are: {0} and case-insensitive".format(supported_values))
 
     return decision, trigger_action
