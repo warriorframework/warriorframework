34ea82a9cf7268ce12b20b515907f18bff3742d3
keenan35i <zane1111@icloud.com>
2017-06-27 12:09:54 -0500

    fixed spacing

--- warrior/WarriorCore/Classes/html_results_class.py
+++ warrior/WarriorCore/Classes/html_results_class.py
@@ -26,16 +26,16 @@ __author__ = 'Keenan Jabri'
 class lineResult():
     data = {}
     html = ''
-
-    def setDynamicContent( self, line ):
+			
+    def setDynamicContent(self, line):
         self.data['dynamic'] = [ line.get("keywords"), line.get("passes"), line.get("failures"), line.get("errors"), line.get("exceptions"), line.get("skipped") ]
         self.data['timestamp'] = line.get("timestamp")
 
-    def setAttributes( self, line, type, stepcount ):
+    def setAttributes(self, line, type, stepcount):
         if 'Keyword' not in type and 'step' not in type:
             stepcount = ''
         self.data = {'nameAttr' : type + 'Record',
-                     'type': type.replace( 'Test', '' ).replace( 'Keyword', 'step ' ) + str(stepcount),
+                     'type': type.replace('Test', '').replace('Keyword', 'step ') + str(stepcount),
                      'name': line.get("name"),
                      'info': line.get("title"),
                      'timestamp': line.get("timestamp"),
@@ -48,10 +48,10 @@ class lineResult():
                     }
         self.keys = [ 'type', 'name', 'info', 'timestamp', 'duration', 'status', 'impact', 'onerror', 'msc', 'static', 'dynamic' ]
 
-    def setHTML( self, line, type, stepcount ):
+    def setHTML(self, line, type, stepcount):
         if self.html == '':
-            self.setAttributes( line, type, stepcount )
-        self.setDynamicContent( line )
+            self.setAttributes(line, type, stepcount)
+        self.setDynamicContent(line)
         topLevel = ''
         topLevelNext = ''
         if self.data['nameAttr'] != 'KeywordRecord':
@@ -77,50 +77,50 @@ class WarriorHtmlResults():
     lineCount = 0
     steps = 0
 
-    def __init__( self, junit_file=None ):
+    def __init__(self, junit_file=None):
         self.junit_file = junit_file
         self.html_template = "{0}{1}Reporting{1}html_results_template.html" \
             .format(Tools.__path__[0], os.sep)
         self.junit_root = xml_Utils.getRoot(self.junit_file)
 
-    def createLineResult( self, line, type ):
+    def createLineResult(self, line, type):
         if self.lineCount >= len(self.lineObjs):
             temp = lineResult()
-            temp.setHTML( line, type, self.steps )
-            self.lineObjs.append( temp )
+            temp.setHTML(line, type, self.steps)
+            self.lineObjs.append(temp)
             self.lineCount += 1
         else:
-            self.lineObjs[ self.lineCount ].setHTML( line, type, self.steps )
+            self.lineObjs[ self.lineCount ].setHTML(line, type, self.steps)
 
-    def setLineObjs( self ):
+    def setLineObjs(self):
         self.lineCount = 0
         project_node_list = [self.junit_root]
         for project_node in project_node_list:
-            self.createLineResult( project_node, "Project" )
+            self.createLineResult(project_node, "Project")
             for testsuite_node in project_node.findall("testsuite"):
-                self.createLineResult( testsuite_node, "Testsuite"  )
+                self.createLineResult(testsuite_node, "Testsuite" )
                 for testcase_node in testsuite_node.findall("testcase"):
-                    self.createLineResult(testcase_node, "Testcase" )
+                    self.createLineResult(testcase_node, "Testcase")
                     self.steps = 0
                     for step_node in testcase_node.findall("properties"):
                         for node in step_node.findall("property"):
                             if node.get('type') == 'keyword':
                                 self.steps += 1
-                                self.createLineResult( node, "Keyword" )
+                                self.createLineResult(node, "Keyword")
 
-    def getPath( self ):
-        filename = file_Utils.getNameOnly(os.path.basename( self.junit_file ))
-        filename = filename.split( "_junit" )[0]
+    def getPath(self):
+        filename = file_Utils.getNameOnly(os.path.basename(self.junit_file))
+        filename = filename.split("_junit")[0]
         html_filename = filename + ".html"
-        if hasattr( self, 'givenPath' ):
+        if hasattr(self, 'givenPath'):
             html_results_path = self.givenPath + os.sep + html_filename
         else:
-            results_dir = os.path.dirname( self.junit_file )
+            results_dir = os.path.dirname(self.junit_file)
             html_results_path = results_dir + os.sep + html_filename
 
         return html_results_path
 
-    def mergeHTML( self, dynamicHTML ):
+    def mergeHTML(self, dynamicHTML):
         temp = open(self.html_template)
         templateHTML = temp.read().replace('\n', '')
         temp.close()
@@ -129,13 +129,13 @@ class WarriorHtmlResults():
 
     def getWarVersion(self):
         path = self.getPath().split('warriorframework')[0] + 'warriorframework/version.txt'
-        if os.path.isfile( path ):
-            version = open( path, 'r').read().split(':')[2]
+        if os.path.isfile(path):
+            version = open(path, 'r').read().split(':')[2]
             return '<div class="version">' + version + '</div>'
         else:
             return ''
 
-    def generateHTML( self, junitObj, givenPath ):
+    def generateHTML(self, junitObj, givenPath):
         if junitObj:
             self.junit_file = junitObj
             self.junit_root = xml_Utils.getRoot(self.junit_file)
@@ -146,10 +146,10 @@ class WarriorHtmlResults():
         html = ''
         for item in self.lineObjs:
             html += item.html
-        html = self.mergeHTML( html )
+        html = self.mergeHTML(html)
 
-        file = open( self.getPath(), 'w' )
-        file.write( html )
+        file = open(self.getPath(), 'w')
+        file.write(html)
         file.close()
 
         print_info("++++ Results Summary ++++")
