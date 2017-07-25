8f9abca968c2355da4fa3138dc45da29ad70b111
kaheichan <kaheichan@utexas.edu>
2017-07-25 17:37:01 -0500

    fix return statement

--- warrior/WarriorCore/step_driver.py
+++ warrior/WarriorCore/step_driver.py
@@ -242,7 +242,7 @@ def execute_step(step, step_num, data_repository, system_name, kw_parallel, queu
         queue.put((keyword_status, kw_resultfile,
                    step_impact.upper(), tc_junit_object))
     elif data_repository['tc_parallel']:
-        pass
+        return keyword_status, kw_resultfile, step_impact, exec_type_onerror
     else:
         # Get the type of the file being executed by Warrior: Case/Suite/Project
         war_file_type = data_repository.get('war_file_type')
