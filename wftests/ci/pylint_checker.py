c487aef0f4887272ad4e71b2ea075a856d669531
kaheichan <kaheichan@utexas.edu>
2017-07-27 16:23:19 -0500

    initialize work

---
+++ wftests/ci/pylint_checker.py
@@ -0,0 +1,6 @@
+import os, sys, subprocess
+
+# subprocess.call("git clone https://github.com/warriorframework/warriorframework.git pylint_warrior", shell=True)
+# os.chdir("pylint_warrior")
+# subprocess.call("git checkout {}".format(sys.argv[1]), shell=True)
+# subprocess.call("git merge --no-edit origin/develop", shell=True)
