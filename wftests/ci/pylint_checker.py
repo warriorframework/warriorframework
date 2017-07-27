524bb9d6d0687826a5fbcb4efb45872319f8a617
kaheichan <kaheichan@utexas.edu>
2017-07-27 16:56:09 -0500

    fixing file path

--- wftests/ci/pylint_checker.py
+++ wftests/ci/pylint_checker.py
@@ -4,4 +4,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         filelist = open(sys.argv[1]).readlines()
         filelist = [x.strip() for x in filelist]
-        print filelist
+        for fi in filelist:
+            if fi.endswith(".py"):
+                print "this file is changed: ", fi
