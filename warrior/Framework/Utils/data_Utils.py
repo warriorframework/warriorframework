3798c1c3eb713d36099e33eec52ad0bbb02f6544
sudharshan, hariprasad <hariprasad.sudharshan@us.fujitsu.com>
2017-08-04 02:34:19 +0530

    address review comments and improvements

--- warrior/Framework/Utils/data_Utils.py
+++ warrior/Framework/Utils/data_Utils.py
@@ -1269,9 +1269,9 @@ def resolve_argument_value_to_get_tag_value(datafile, system_name,
     then value is returned.
 
     """
-    if element_value_in_argument.startswith("tag"):
+    if element_value_in_argument.startswith("tag="):
         tag_name = evaluate_tc_argument_value(element_value_in_argument)
-        if tag_name is not False:
+        if tag_name:
             system_name_list = xml_Utils.get_matching_firstlevel_children_from_root(datafile, 
                                                                                     "system")
             if system_name_list == [] or system_name_list is None or system_name_list is False:
@@ -1282,11 +1282,18 @@ def resolve_argument_value_to_get_tag_value(datafile, system_name,
                         node_list = xml_Utils.get_matching_firstlevel_children_from_node(system, 
                                                                                          tag_name)
                         if node_list == [] or node_list is None or node_list is False:
+                            print_error("The tag value: {0} is not defined in the datafile:{1}"\
+                                .format(tag_name, datafile))
                             return False
                         else:
-                            return node_list[0].text
+                            tag_value = node_list[0].text
+                            tag_value = sub_from_env_var(tag_value)
+                            tag_value = sub_from_data_repo(tag_value)
+                            return tag_value
                 return element_value_in_argument
         else:
+            print_error("The value for arg {0} is not defined in the case".
+                        format(element_value_in_argument))
             return element_value_in_argument
     else:
         return element_value_in_argument
@@ -1309,8 +1316,6 @@ def get_user_specified_tag_values_in_tc(datafile, system_name, **kwargs):
     for element in kwargs:
         if kwargs[element] is not None:
             credentials[element] = resolve_argument_value_to_get_tag_value(datafile, system_name, kwargs[element])
-    credentials = sub_from_env_var(credentials)
-    credentials = sub_from_data_repo(credentials)
     return credentials
 
 
