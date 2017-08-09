c3736916da0dcd2bf856a1e0ca16fa1ee09b5c1b
kaheichan <kaheichan@utexas.edu>
2017-08-09 17:27:33 -0500

    using subprocess check_output to capture pylint output

--- wftests/ci/pylint_checker.py
+++ wftests/ci/pylint_checker.py
@@ -1,4 +1,5 @@
 import os, sys, subprocess
+import StringIO
 from pylint import epylint as lint
 
 if __name__ == "__main__":
@@ -10,8 +11,13 @@ if __name__ == "__main__":
         ignore = [x for x in pylintrc if x.startswith("ignore=")]
         if ignore:
             ignore = ignore[0][7:].replace('\n', '').split(',')
-            print ignore
+            print "Ignoring", ', '.join(ignore)
         for fi in filelist:
             if fi.endswith(".py") and all([x not in fi for x in ignore]):
-                print "this file is changed: ", fi
-                lint.py_run('{} --rcfile=../../.pylintrc'.format(fi))
+                print "Pylint is running on:", fi
+                # lint.py_run('{} --rcfile=../../.pylintrc'.format(fi))
+                try:
+                    output = subprocess.check_output('pylint --rcfile=.pylintrc {}'.format(fi), shell=True)
+                except subprocess.CalledProcessError as e:
+                    output = e.output
+                print output
