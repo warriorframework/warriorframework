3bcea566532336a0df3819acfc4cca7a49007361
kaheichan <kaheichan@utexas.edu>
2017-08-10 10:09:22 -0500

    better print in custom rules

--- wftests/ci/custom_rules.py
+++ wftests/ci/custom_rules.py
@@ -103,6 +103,7 @@ def main(kw=False):
         scan for function - function check
     """
     f = open(sys.argv[1])
+    print "\n**********", sys.argv[1]
     if "Actions/" in sys.argv:
         kw = True
     root = ast.parse(f.read())
