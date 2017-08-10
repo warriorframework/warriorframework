fe63d092e727476eed44ed01ec3a789eca7df125
kaheichan <kaheichan@utexas.edu>
2017-08-09 21:20:55 -0500

    cleaning extra files, trigger job on Travis

--- wftests/ci/pylint.sh
+++ wftests/ci/pylint.sh
@@ -1,27 +1,20 @@
-TRAVIS_BRANCH=develop
-TRAVIS_PULL_REQUEST_BRANCH=feature/WAR-1336
-# rm -rf ../pylint_warrior
-
 # set -x
-# pip install pylint
+pip install pylint
 
 cd ../
-# git clone https://github.com/warriorframework/warriorframework.git pylint_warrior
+git clone https://github.com/warriorframework/warriorframework.git pylint_warrior
 cd pylint_warrior
 
-# git checkout "${TRAVIS_BRANCH}"
-# git checkout "${TRAVIS_PULL_REQUEST_BRANCH}"
-# echo "Merging ${TRAVIS_BRANCH} into ${TRAVIS_PULL_REQUEST_BRANCH}"
-# git merge --no-edit "${TRAVIS_BRANCH}"
-# git add .
+git checkout "${TRAVIS_BRANCH}"
+git checkout "${TRAVIS_PULL_REQUEST_BRANCH}"
+echo "Merging ${TRAVIS_BRANCH} into ${TRAVIS_PULL_REQUEST_BRANCH}"
+git merge --no-edit "${TRAVIS_BRANCH}"
+git add .
 
-# git branch
+git branch
 # Displaying what .py files have changed
 filelist=$(git --no-pager diff "${TRAVIS_BRANCH}" --name-only | grep ".py$")
 if [[ "$filelist" ]]; then
-    # echo "List of .py files that have changed in this commit"
-    # echo "$filelist"
-    # echo ""
     echo "$filelist" > filelist.txt
     python ../warriorframework/wftests/ci/pylint_checker.py filelist.txt .pylintrc "${TRAVIS_BRANCH}" "${TRAVIS_PULL_REQUEST_BRANCH}"
 else
