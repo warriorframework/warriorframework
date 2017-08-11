1fb5980d815a0216dfb840f6a7c493a7e844b660
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2017-08-11 12:30:07 -0500

    Added support for localhost:5000 url to be re-directed to localhost:5000/katana, added common_templates directory

--- katana/wui/urls.py
+++ katana/wui/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^katana/', include('wui.core.urls')),
+    url(r'^$', RedirectView.as_view(url='/katana/')),
     url(r'^katana/settings/', include('native.settings.urls')),
 ]
