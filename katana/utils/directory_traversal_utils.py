10f15aa4acdda1085d81945584786e4bb5f62efe
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2017-08-22 13:39:33 -0500

    Minor bugfixes

--- katana/utils/directory_traversal_utils.py
+++ katana/utils/directory_traversal_utils.py
@@ -120,6 +120,7 @@ def get_paths_of_subfiles(parent_dir, extension=re.compile("\..*")):
         file_path: Returns a list of paths to sub-files inside the parent_dir
 
     """
+    print parent_dir
     file_paths = []
     sub_files_and_folders = get_sub_dirs_and_files(parent_dir, abs_path=True)
     for sub_file in sub_files_and_folders["files"]:
@@ -127,6 +128,7 @@ def get_paths_of_subfiles(parent_dir, extension=re.compile("\..*")):
             file_paths.append(sub_file)
     for sub_folder in sub_files_and_folders["folders"]:
         file_paths.extend(get_paths_of_subfiles(sub_folder, extension=extension))
+    print file_paths
     return file_paths
 
 
@@ -154,11 +156,7 @@ def join_path(path, *paths):
     Returns:
         Joined path
     """
-    joined_path = os.path.join(path, *paths)
-    if os.path.exists(joined_path):
-        return joined_path
-    else:
-        return False
+    return os.path.join(path, *paths)
 
 
 def get_relative_path(path, start_directory):
