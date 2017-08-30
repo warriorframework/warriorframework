a75fcf9304787cab8ea1408d435796725b2c5f4f
Kamran Husain <Kamran.Husain@us.fujitsu.com>
2017-08-30 13:07:18 -0500

    Revert "Added Cases too"
    This reverts commit eb38c2d2c76c5fbf67af4f7d2387151ef9261ab1.

--- katana/native/cases/urls.py
+++ katana/native/cases/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
- 	url(r'editCase', views.editCase , name='editCase'),
-    
+    url(r'^email_setting_handler', views.email_setting_handler, name='email_setting_handler'),
+    url(r'^secret_handler', views.secret_handler, name='secret_handler'),
+    url(r'^jira_setting_handler', views.jira_setting_handler, name='jira_setting_handler'),
 ]
