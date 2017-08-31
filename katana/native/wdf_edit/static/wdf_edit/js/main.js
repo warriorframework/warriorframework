638e770335abf2dcf6bce58d1528dff63b99becd
kaheichan <kaheichan@utexas.edu>
2017-08-31 16:44:00 -0500

    further integration into ui

--- katana/native/wdf_edit/static/wdf_edit/js/main.js
+++ katana/native/wdf_edit/static/wdf_edit/js/main.js
@@ -1,19 +1,9 @@
-$(document).ready(function(){
+var wdf = {
+    toggle: function(){
+        $(this).parent().parent().parent().children("#content").toggle();
+    },
 
-    // $.get("json", function(data, status){
-    //     console.log(JSON.stringify(data, null, 2));
-    // });
-
-    $("#add").click(function(){
-        console.log($("#system_template").html());
-        $("#big-box").append($($("#system_template").html()));
-    });
-
-    $(".toggle").click(function(){
-        $(this).parent().parent().children(".row").toggle();
-    });
-
-    $("#submit").click(function(){
+    submit: function(){
         var csrftoken = $("[name='csrfmiddlewaretoken']").val();
 
         $.ajax({
@@ -27,6 +17,15 @@ $(document).ready(function(){
                 // console.log(data);
             }
         }); 
-    });
+    },
 
-});
+
+    add: function(){
+        console.log($("#system_template").html());
+        $("#big-box").append($($("#system_template").html()));
+    },
+
+    addtag: function(){
+        $(this).parent().parent().last(".row").after($($("#tag_template").html()));
+    },
+}
