3c0a4130c96df36678e93666e5a29458830de3bc
kaheichan <kaheichan@utexas.edu>
2017-09-01 15:02:07 -0500

    correctly create new tag and system element

--- katana/native/wdf_edit/static/wdf_edit/js/main.js
+++ katana/native/wdf_edit/static/wdf_edit/js/main.js
@@ -21,12 +21,25 @@ var wdf = {
 
 
     add: function(){
-        var $tmp = $("#system_template");
+        var $tmp = $("#system_template").clone();
+        $tmp.find("#template-system").prop("id", $(".control-box").length+"-control-box")
+        $tmp.find("[name='template-system-name']").prop("name", $(".control-box").length+"-system_name");
+        $tmp.find("[name='template-system.tag']").prop("name", $(".control-box").length+"-1-key");
+        $tmp.find("[name='template-system.value']").prop("name", $(".control-box").length+"-1-value");
         console.log($tmp.html());
-        // $("#big-box").append($($("#system_template").html()));
+        $("#big-box").append($($tmp.html()));
     },
 
     addtag: function(){
-        $(this).parent().parent().parent().append($($("#tag_template").html()));
+        var $tmp = $("#tag_template").clone();
+        var $target = $(this).parent().parent().parent();
+        var $count = $target.attr("id").substring(0, $target.attr("id").length-11);
+        $tmp.find("[name='template-tag.tag']").prop("name", $count+($target.children("#content").length+1)+"-key");
+        $tmp.find("[name='template-tag.value']").prop("name", $count+($target.children("#content").length+1)+"-value");
+        $target.append($($tmp.html()));
     },
+
+    hide: function(){
+        $(this).hide();
+    }
 }
