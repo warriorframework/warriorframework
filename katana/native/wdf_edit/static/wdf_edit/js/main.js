783c63b69cdf770177a0903c42308d5193a827a5
kaheichan <kaheichan@utexas.edu>
2017-09-06 14:51:38 -0500

    add subsystem, delete system and tag

--- katana/native/wdf_edit/static/wdf_edit/js/main.js
+++ katana/native/wdf_edit/static/wdf_edit/js/main.js
@@ -1,6 +1,15 @@
 var wdf = {
     toggle: function(){
+        // hide all the div with id content under control-box
         $(this).parent().parent().parent().children("#content").toggle();
+    },
+
+    deleteTag: function(){
+        $(this).parent().parent().empty();
+    },
+
+    deleteSystem: function(){
+        $(this).parent().parent().parent().empty();
     },
 
     submit: function(){
@@ -13,6 +22,7 @@ var wdf = {
             headers: {'X-CSRFToken':csrftoken},
             //contentType: 'application/json',
             success: function(data){
+                // load the tree
                 katana.$activeTab.find("#main_info").replaceWith(data);
                 katana.refreshAutoInit(katana.$activeTab.find("#jstree"));
             }
@@ -21,20 +31,35 @@ var wdf = {
 
 
     add: function(){
+        // Add a system
         var $tmp = katana.$activeTab.find("#system_template").clone();
-        $tmp.find("#template-system").prop("id", katana.$activeTab.find(".control-box").length+"-control-box")
-        $tmp.find("[name='template-system-name']").prop("name", katana.$activeTab.find(".control-box").length+"-system_name");
-        $tmp.find("[name='template-system.tag']").prop("name", katana.$activeTab.find(".control-box").length+"-1-key");
-        $tmp.find("[name='template-system.value']").prop("name", katana.$activeTab.find(".control-box").length+"-1-value");
+        $tmp.find("#template-system").prop("id", katana.$activeTab.find(".control-box").length-1+"-control-box")
+        $tmp.find("[name='template-system-name']").prop("name", katana.$activeTab.find(".control-box").length-1+"-system_name");
+        $tmp.find("[name='template-system.tag']").prop("name", katana.$activeTab.find(".control-box").length-1+"-1-key");
+        $tmp.find("[name='template-system.value']").prop("name", katana.$activeTab.find(".control-box").length-1+"-1-value");
         katana.$activeTab.find("#big-box").append($($tmp.html()));
     },
 
     addtag: function(){
         var $tmp = katana.$activeTab.find("#tag_template").clone();
+        // go to control box level
         var $target = $(this).parent().parent().parent();
-        var $count = $target.attr("id").substring(0, $target.attr("id").length-11);
-        $tmp.find("[name='template-tag.tag']").prop("name", $count+($target.children("#content").length+1)+"-key");
-        $tmp.find("[name='template-tag.value']").prop("name", $count+($target.children("#content").length+1)+"-value");
+        var $id = $target.attr("id").substring(0, $target.attr("id").length-11);
+        $tmp.find("[name='template-tag.tag']").prop("name", $id+($target.children("#content").length+1)+"-key");
+        $tmp.find("[name='template-tag.value']").prop("name", $id+($target.children("#content").length+1)+"-value");
+        $target.append($($tmp.html()));
+    },
+
+    addSubSystem: function(){
+        var $tmp = katana.$activeTab.find("#subsystem_template").clone();
+        var $target = $(this).parent().parent().parent();
+        var $system_id = $target.attr("id").split("-")[0];
+        var $subsystem_count = $target.attr("id").split("-")[1];
+        $tmp.find("[name='template-system-name']").attr("value", $target.find('[name*="system_name"]').attr("value"));
+        $tmp.find("[name='template-system-name']").prop("name", $system_id+"-"+($subsystem_count+1)+"-system_name");
+        $tmp.find("[name='template-subsystem-name']").prop("name", $system_id+"-"+($subsystem_count+1)+"-subsystem_name");
+        $tmp.find("[name='template-tag.tag']").prop("name", $system_id+"-"+($subsystem_count+1)+"-"+($target.children("#content").length+1)+"-key");
+        $tmp.find("[name='template-tag.value']").prop("name", $system_id+"-"+($subsystem_count+1)+"-"+($target.children("#content").length+1)+"-value");
         $target.append($($tmp.html()));
     },
 
@@ -68,6 +93,7 @@ var wdf = {
                         katana.$activeTab.find("#main_info").replaceWith(data);
                         katana.refreshAutoInit(katana.$activeTab.find("#system_template"));
                         katana.refreshAutoInit(katana.$activeTab.find("#tag_template"));
+                        katana.refreshAutoInit(katana.$activeTab.find("#subsystem_template"));
                         // console.log("loaded");
                     }
                 });
