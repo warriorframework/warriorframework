2c456e1d247fa9100a75e7707ef2a4bae8dcb5bf
keenan35i <zane1111@icloud.com>
2017-09-07 13:24:25 -0500

    Added Profile Settings

--- katana/native/settings/urls.py
+++ katana/native/settings/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^secret_handler', views.secret_handler, name='secret_handler'),
     url(r'^jira_setting_handler', views.jira_setting_handler, name='jira_setting_handler'),
     url(r'^general_setting_handler', views.general_setting_handler, name='general_setting_handler'),
+    url(r'^profile_setting_handler', views.profile_setting_handler, name='profile_setting_handler'),
 ]
