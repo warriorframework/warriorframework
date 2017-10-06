55071f1df406344a0a73eab7938c3c30ec83a055
Kamran Husain <kamran.husain@us.fujitsu.com>
2017-10-06 13:20:44 -0500

    Test with small startup script and simple instructions

--- katana/startKatana.bash
+++ katana/startKatana.bash
@@ -5,6 +5,7 @@ python -c 'import django'
 result="$?"
 if [ "$result" -ne 0 ]; then 
 	echo "Please install django for katana to work"
+	echo "Try sudo pip install Django"
 else 
 	python ./katana.py
 fi
