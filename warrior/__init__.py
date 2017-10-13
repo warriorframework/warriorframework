3b7c5cd5bf61e012efe5c814239999a1625a0274
Kamran Husain <kamran.husain@us.fujitsu.com>
2017-10-13 13:39:49 -0500

    Make warrior a package. Run setup.py to install

---
+++ warrior/__init__.py
@@ -0,0 +1,24 @@
+'''
+Copyright 2017, Fujitsu Network Communications, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+ +distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+import warrior.Framework
+import warrior.Framework as Framework
+import warrior.Framework.Utils 
+import warrior.Framework.Utils as Utils 
+import warrior.Tools 
+import warrior.Tools as Tools
+import warrior.WarriorCore
+import warrior.WarriorCore as WarriorCore
+import warrior.Actions
+import warrior.Actions as Actions
+import warrior.ProductDrivers
+import warrior.ProductDrivers as ProductDrivers
