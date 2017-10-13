dc5f8566df23c7c62970616e3b99fa91f7ef8747
Kamran Husain <kamran.husain@us.fujitsu.com>
2017-10-13 09:58:10 -0500

    Added call to fix max stdout buffer issue with electron

--- katana/main.js
+++ katana/main.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-
+// We must load jquery. 
 let win
 let port = ''+5003
 
@@ -43,20 +43,31 @@ app.on('activate', () => {
 let pyProc = null
 let pyPort = null
 
+var execute = function(command, callback){
+    exec(command, {maxBuffer: 1024 * 1000}, function(error, stdout, stderr){ callback(error, stdout); });
+};
+
+var executeFile = function(command, callback){
+    exec(command, {maxBuffer: 1024 * 1000}, function(error, stdout, stderr){ callback(error, stdout); });
+};
 
 const createPyProc = () => {
   let script = path.join(__dirname, 'katana.py')
   console.log(script, port)
  // pyProc = require('child_process').spawn('/usr/bin/python', [script, port])
-  pyProc = require('child_process').execFile('/usr/bin/python', [script, '-p', port], (error,stdout,stderr) => {
-	  if (error) {
+
+
+ //pyProc = require('child_process').execFile('/usr/bin/python', [script, '-p', port], (error,stdout,stderr) => {
+  pyProc = require('child_process').exec(['/usr/bin/python '+ script + ' -p ' + port],  {maxBuffer: 1024 * 1000}, (error,stdout,stderr) => {
+
+  if (error) {
 		  throw error;
 	  }
   })
   //pyProc = require('child_process').exec(script) 
   if (pyProc != null) {
     console.log('child process success')
-    win.loadURL("http://localhost:"+port+"/katana");
+   //win.loadURL("http://localhost:"+port+"/katana");
   }
 }
 
