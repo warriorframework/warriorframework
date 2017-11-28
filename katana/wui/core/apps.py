a9d7f44d0356675f589edbd8a751bf2da84974e2
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2017-11-28 13:58:44 -0600

    Code cleanup

--- katana/wui/core/apps.py
+++ katana/wui/core/apps.py
@@ -38,76 +38,83 @@ class CoreConfig(AppConfig):
                                              'settings_apps': settings_apps})
 
         warrior_dir = nav_obj.get_warrior_dir()
-        ordered_json = OrderedDict()
-        with open(os.path.join(base_directory, "config.json"), "r") as f:
+        config_json_file = os.path.join(base_directory, "config.json")
+        with open(config_json_file, "r") as f:
             json_data = json.load(f)
-        if "engineer" not in json_data:
-            ordered_json["engineer"] = ""
-        else:
-            ordered_json["engineer"] = json_data["engineer"]
 
-        ordered_json["pythonsrcdir"] = warrior_dir[:-1] \
-            if "pythonsrcdir" not in json_data or json_data["pythonsrcdir"] == "" \
-            else json_data["pythonsrcdir"]
+        ordered_json = validate_config_json(json_data, warrior_dir)
 
-        warrior_dir = ordered_json["pythonsrcdir"]
-
-        if "xmldir" not in json_data or json_data["xmldir"] == "":
-            path = get_abs_path(join_path("Warriorspace", "Testcases"), warrior_dir)
-            if path is not None:
-                ordered_json["xmldir"] = path
-            else:
-                ordered_json["xmldir"] = ""
-                print "-- An Error Occurred -- Path to Cases directory could not be located"
-        else:
-            ordered_json["xmldir"] = json_data["xmldir"]
-
-        if "testsuitedir" not in json_data or json_data["testsuitedir"] == "":
-            path = get_abs_path(join_path("Warriorspace", "Suites"), warrior_dir)
-            if path is not None:
-                ordered_json["testsuitedir"] = path
-            else:
-                ordered_json["testsuitedir"] = ""
-                print "-- An Error Occurred -- Path to Cases directory could not be located"
-        else:
-            ordered_json["testsuitedir"] = json_data["testsuitedir"]
-
-        if "projdir" not in json_data or json_data["projdir"] == "":
-            path = get_abs_path(join_path("Warriorspace", "Projects"), warrior_dir)
-            if path is not None:
-                ordered_json["projdir"] = path
-            else:
-                ordered_json["projdir"] = ""
-                print "-- An Error Occurred -- Path to Cases directory could not be located"
-        else:
-            ordered_json["projdir"] = json_data["projdir"]
-
-        if "idfdir" not in json_data or json_data["idfdir"] == "":
-            path = get_abs_path(join_path("Warriorspace", "Data"), warrior_dir)
-            if path is not None:
-                ordered_json["idfdir"] = path
-            else:
-                ordered_json["idfdir"] = ""
-                print "-- An Error Occurred -- Path to Cases directory could not be located"
-        else:
-            ordered_json["idfdir"] = json_data["idfdir"]
-
-        if "testdata" not in json_data or json_data["testdata"] == "":
-            path = get_abs_path(join_path("Warriorspace", "Config_files"), warrior_dir)
-            if path is not None:
-                ordered_json["testdata"] = path
-            else:
-                ordered_json["testdata"] = ""
-                print "-- An Error Occurred -- Path to Cases directory could not be located"
-        else:
-            ordered_json["testdata"] = json_data["testdata"]
-
-        if "pythonpath" not in json_data:
-            ordered_json["pythonpath"] = ""
-        else:
-            ordered_json["pythonpath"] = json_data["pythonpath"]
-
-        with open(os.path.join(base_directory, "config.json"), "w") as f:
+        with open(config_json_file, "w") as f:
             f.write(json.dumps(ordered_json, indent=4))
 
         # print "***************You are in Core Katana App Config Class***************"
+
+
+def validate_config_json(json_data, warrior_dir):
+    ordered_json = OrderedDict()
+    if "engineer" not in json_data:
+        ordered_json["engineer"] = ""
+    else:
+        ordered_json["engineer"] = json_data["engineer"]
+
+    ordered_json["pythonsrcdir"] = warrior_dir[:-1] \
+        if "pythonsrcdir" not in json_data or json_data["pythonsrcdir"] == "" \
+        else json_data["pythonsrcdir"]
+
+    warrior_dir = ordered_json["pythonsrcdir"]
+
+    if "xmldir" not in json_data or json_data["xmldir"] == "":
+        path = get_abs_path(join_path("Warriorspace", "Testcases"), warrior_dir)
+        if path is not None:
+            ordered_json["xmldir"] = path
+        else:
+            ordered_json["xmldir"] = ""
+            print "-- An Error Occurred -- Path to Cases directory could not be located"
+    else:
+        ordered_json["xmldir"] = json_data["xmldir"]
+
+    if "testsuitedir" not in json_data or json_data["testsuitedir"] == "":
+        path = get_abs_path(join_path("Warriorspace", "Suites"), warrior_dir)
+        if path is not None:
+            ordered_json["testsuitedir"] = path
+        else:
+            ordered_json["testsuitedir"] = ""
+            print "-- An Error Occurred -- Path to Cases directory could not be located"
+    else:
+        ordered_json["testsuitedir"] = json_data["testsuitedir"]
+
+    if "projdir" not in json_data or json_data["projdir"] == "":
+        path = get_abs_path(join_path("Warriorspace", "Projects"), warrior_dir)
+        if path is not None:
+            ordered_json["projdir"] = path
+        else:
+            ordered_json["projdir"] = ""
+            print "-- An Error Occurred -- Path to Cases directory could not be located"
+    else:
+        ordered_json["projdir"] = json_data["projdir"]
+
+    if "idfdir" not in json_data or json_data["idfdir"] == "":
+        path = get_abs_path(join_path("Warriorspace", "Data"), warrior_dir)
+        if path is not None:
+            ordered_json["idfdir"] = path
+        else:
+            ordered_json["idfdir"] = ""
+            print "-- An Error Occurred -- Path to Cases directory could not be located"
+    else:
+        ordered_json["idfdir"] = json_data["idfdir"]
+
+    if "testdata" not in json_data or json_data["testdata"] == "":
+        path = get_abs_path(join_path("Warriorspace", "Config_files"), warrior_dir)
+        if path is not None:
+            ordered_json["testdata"] = path
+        else:
+            ordered_json["testdata"] = ""
+            print "-- An Error Occurred -- Path to Cases directory could not be located"
+    else:
+        ordered_json["testdata"] = json_data["testdata"]
+
+    if "pythonpath" not in json_data:
+        ordered_json["pythonpath"] = ""
+    else:
+        ordered_json["pythonpath"] = json_data["pythonpath"]
+    return ordered_json
