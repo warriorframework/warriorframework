08269e7cd85e04b2ab45619d455e1639acd97344
SATHYAMOORTHY RADHAKRISHNAN <sathya.radhakrishnan@us.fujitsu.com>
2018-01-31 15:50:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     version=PACKAGE_VERSION,
     author="warriorframework org",
     url="https://github.com/warriorframework/warriorframework",
-    install_requires=["pexpect==4.2", "requests==2.9.1", "selenium==2.53.0"],
+    install_requires=["pexpect==4.2", "requests==2.9.1", "selenium==2.53.0", "lxml==3.3.3",
+                     "paramiko==1.16.0", "pysnmp==4.3.2", "pyvirtualdisplay==0.2.1"],
 
 )
