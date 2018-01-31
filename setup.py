ddd06c4c80a48d5f44ca1dae6d08e4b1f20b7fbe
SATHYAMOORTHY RADHAKRISHNAN <sathya.radhakrishnan@us.fujitsu.com>
2018-01-31 15:47:51 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup, find_packages
+"""
+setup file for warriorframework
+"""
+from setuptools import setup
 
-package_name = "warriorframework"
-package_version = "3.7.0"
+PACKAGE_NAME = "warriorframework"
+PACKAGE_VERSION = "3.7.0"
 
 setup(
-    name=package_name,
-    version=package_version,
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
     author="warriorframework org",
     url="https://github.com/warriorframework/warriorframework",
     install_requires=["pexpect==4.2", "requests==2.9.1", "selenium==2.53.0"],
