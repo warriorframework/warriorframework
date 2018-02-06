573a5ea48b238b6b9c5f738b41007a8c3740472d
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2018-02-06 11:17:19 -0600

    Code formatting and code restructuring

--- katana/katana.py
+++ katana/katana.py
@@ -11,67 +11,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 """
-
-import os
 import sys
+import os
+from simple_server import main
+use_py_server = False
 
-usePythonServer = 0
-
-####
-# Check number 1: Don't run the server if django not installed. 
-# 
 try:
     import django
-except: 
-    print """
-    You are missing the django framework. 
-    Please install django on you system. 
-    Try  sudo pip install django 
-
-    Refer to this link for more information 
-    https://docs.djangoproject.com/en/1.11/topics/install/
-    """
-    usePythonServer = 1; 
-
-import sys
-import BaseHTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
-
-warningMessage="""
-<H1>You are not running django! </H1>
-Please make sure you are running katana with django installed for the server. <p>
-<a href="https://docs.djangoproject.com/en/1.11/topics/install/">Documentation for installing django here.</a>
-""";
-
-HandlerClass = SimpleHTTPRequestHandler
-ServerClass  = BaseHTTPServer.HTTPServer
-Protocol     = "HTTP/1.0"
-REDIRECTIONS = {"/": "http://python.org/",
-                "/katana/": "http://google.com/"}
-LAST_RESORT = "http://google.com/"  
-
-class SimpleHandler(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body>%s</body></html>" % warningMessage)
-
-    def do_HEAD(self):
-        self.send_response(301)
-        self.send_header("Location", REDIRECTIONS.get(self.path, LAST_RESORT))
-        self.end_headers()
-
-    def do_POST(self):
-        # Doesn't do anything with posted data
-        self._set_headers()
-        self.wfile.write("<html><body>%s</body></html>" % warningMessage)
-        # self.wfile.write("<html><body><h1>DONT POST!</h1></body></html>")
+except ImportError:
+    use_py_server = True
+except Exception as e:
+    print "-- An Error Occurred -- {0}".format(e)
 
 
 if __name__ == "__main__":
@@ -116,19 +66,8 @@ if __name__ == "__main__":
             else:
                 port = args[i+1]
             break
-    if usePythonServer:
-        server_address = ('127.0.0.1', int(port))
-        HandlerClass.protocol_version = Protocol 
-        httpd = ServerClass(server_address, SimpleHandler)
-        sa = httpd.socket.getsockname()
-        print "WARNING Serving django-less server on", sa[0], "port", sa[1], "..."
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            pass
-        httpd.server_close()
-    else: 
+
+    if not use_py_server:
         os.system("{0} {1} {2} {3}".format(command, filepath, runserver, port))
-    
-    
-    
+    else:
+        main(port)
