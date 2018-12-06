041ea72c87659c483246112afb41b4cbfb5cb24f
Reddy Yadavalli <ryadaval@localhost>
2018-12-05 23:23:39 -0600

    WAR 2029 keyword to log messages

--- warrior/Actions/LogActions/log_actions.py
+++ warrior/Actions/LogActions/log_actions.py
@@ -10,6 +10,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
+import Framework.Utils as Utils
 from Framework.Utils.print_Utils import print_info, print_error, print_debug, print_warning
 from Framework.Utils.data_Utils import get_object_from_datarepository
 
@@ -23,10 +24,16 @@ class LogActions(object):
             Constructor
         """
 
+        self.resultfile = Utils.config_Utils.resultfile
+        self.datafile = Utils.config_Utils.datafile
+        self.logsdir = Utils.config_Utils.logsdir
+        self.filename = Utils.config_Utils.filename
+        self.logfile = Utils.config_Utils.logfile
         self.map_function = {"INFO":print_info, \
                              "DEBUG":print_debug, \
                              "WARN":print_warning, \
                              "ERROR":print_error}
+
     def log_message(self, message=None, type="INFO", list_message=None, dict_message=None):
         """Keyword to print the given message.
            :Arguments:
@@ -39,15 +46,18 @@ class LogActions(object):
                   one of the arguments message, list_message, dict_message is mandatory.
 
            :Returns:
-                  1. status(boolean)
+                  1. True (boolean), this keyword always returns True.
+                     Don't want to fail the test based on this keyword.
 
         """
+        wdesc = "keyword to print the given log message"
+        Utils.testcase_Utils.pNote(wdesc)
         if not (message or list_message or dict_message):
-            print_warning("Please specify atleast one message for printing")
+            print_error("Please specify atleast one message for printing")
             return True
         if not self.map_function.get(type):
             print_error("type : "+type+" is not supported")
-            return False
+            return True
         if message:
             self.map_function[type](message)
         if list_message:
