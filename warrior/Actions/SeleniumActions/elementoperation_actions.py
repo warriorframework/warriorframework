3307f3c11ab7d93b1ac896de626e55214046ba76
apathapa <anil.pathapati@us.fujitsu.com>
2019-05-21 08:31:56 -0500

    commenting selenium failed testcases.

--- warrior/Actions/SeleniumActions/elementoperation_actions.py
+++ warrior/Actions/SeleniumActions/elementoperation_actions.py
@@ -12,11 +12,9 @@ limitations under the License.
 '''
 
 """ Selenium keywords for Element Operation Actions """
-import time
 from Framework.ClassUtils.WSelenium.element_operations import ElementOperations
 from Framework.ClassUtils.json_utils_class import JsonUtils
 from Framework.Utils.list_Utils import get_list_comma_sep_string
-from selenium.webdriver.common.keys import Keys
 
 try:
     import Framework.Utils as Utils
@@ -2053,7 +2051,6 @@ class elementoperation_actions(object):
                 source_comp_locator = browser_details["source_locator_type"] + "=" + browser_details["source_locator"]
                 br_name = system_name + "_" + browser_details["browser_name"]
                 current_browser = Utils.data_Utils.get_object_from_datarepository(br_name)
-                current_browser.set_window_size(1024, 768)
                 if not current_browser:
                         pNote("No browser instance {0} found in the data "
                               "repository!".format(br_name), "error")
